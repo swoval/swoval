a5e7339c46115031f5fa2124cdec9cb0bc6bb504
Ethan Atkins <ethan.atkins@gmail.com>
2017-12-04 16:18:58 -0800

    Improve startup performance
    It turns out that it is quite slow to recursively scan large
    directories, which can lead to long start up times for continuous builds
    for large projects. To fix this, I add some additional concurrency to
    the WatchService. In particular, we do all directory traversals on a
    background thread and post all of the file system callbacks onto this
    same thread.
    
    Because I added additional concurrency, I had to add some additional
    callback functions to the service to make it testable. In particular,
    the service can now generate callbacks when a key is registered (this is
    deferred until after the file system is completed, if necessary), when a
    a key is offered to the service (for poll) and finally whenever a file
    event is created. I also generalized some of the test cases a bit to
    take advantage of these callbacks in a more generic way.
    
    It also is slow, but non-critical, to run the loop that cleans up
    WatchKeys that may no longer need FSEventStreamRefs. To prevent that
    cleanup from interfering with building the file mapping, I run those on
    a separate thread.

--- src/main/scala/com/swoval/watchservice/MacOSXWatchService.scala
+++ src/main/scala/com/swoval/watchservice/MacOSXWatchService.scala
@@ -3,19 +3,18 @@ package com.swoval.watchservice
 import java.io.File
 import java.nio.file.StandardWatchEventKinds.{ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY, OVERFLOW}
 import java.nio.file._
-import java.util.concurrent.{ArrayBlockingQueue, LinkedBlockingDeque, TimeUnit}
+import java.util.concurrent._
 import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
-import java.util.stream.Collectors.toSet
 import java.util.{Collections, List => JList}
 
 import com.sun.jna.{NativeLong, Pointer => Ptr}
 import com.swoval.watchservice.CarbonAPI.INSTANCE._
 import sbt.io.WatchService
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
-import scala.collection.mutable
+import scala.collection.{Set, mutable}
 import scala.concurrent.duration._
-import scala.util.Try
 
 /**
   * Provides the lion's share of the implementation details for receiving file system
@@ -25,11 +24,19 @@ import scala.util.Try
   *
   * @author Ethan Atkins
   */
-class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: WatchKey => Unit)
-  extends WatchService {
+class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(
+  onOffer: WatchKey => Unit = _ => {},
+  onRegister: WatchKey => Unit = _ => {},
+  onEvent: WatchEvent[_] => Unit = _ => {},
+) extends WatchService {
+
+  private[this] val executor = Executors.newSingleThreadExecutor()
+  private[this] val cleanupExecutor = Executors.newSingleThreadExecutor()
 
   override def close(): Unit = {
     if (open.compareAndSet(true, false)) {
+      executor.shutdown()
+      cleanupExecutor.shutdown()
       registered foreach { case (_, k) => cleanupKey(k) }
       CarbonAPI.INSTANCE.CFRunLoopStop(thread.runLoop)
       thread.interrupt()
@@ -40,46 +47,54 @@ class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: Wa
   override def init(): Unit = {}
 
   override def poll(timeout: Duration): WatchKey = {
-    readyKeys.poll(timeout.toMicros, TimeUnit.MICROSECONDS)
+    readyKeys.poll(timeout.toNanos, TimeUnit.NANOSECONDS)
   }
 
   override def pollEvents(): Map[WatchKey, Seq[WatchEvent[Path]]] = {
-    registered.flatMap { case (_, k) =>
+    registered.synchronized(registered.flatMap { case (_, k) =>
       val events = k.pollEvents()
       if (events.isEmpty) None
       else Some(k -> events.asScala.map(_.asInstanceOf[WatchEvent[Path]]))
-    }.toMap[WatchKey, Seq[WatchEvent[Path]]]
+    }).toMap[WatchKey, Seq[WatchEvent[Path]]]
   }
 
-  override def register(path: Path, events: WatchEvent.Kind[Path]*): WatchKey = {
-    registered get path match {
-      case Some(k) => return k;
-      case _ =>
-    }
-    val key = new MacOSXWatchKey(path, createStream(path), queueSize, events: _*)
-    allFiles ++= mutable.Map(recursiveListFiles(path).view.filterNot(allFiles.contains).map { f =>
-      f -> lastModified(f)
-    }.toSeq: _*).groupBy { case (f, _) => f.getParent }
+  override def register(path: Path, events: WatchEvent.Kind[Path]*): WatchKey =
+    registered.synchronized {
+      registered get path match {
+        case Some(k) => return k;
+        case _ =>
+      }
+      val key = new MacOSXWatchKey(path, createStream(path), queueSize, events: _*)
 
-    registered collectFirst { case (p, k) if path startsWith p => k } match {
-      case Some(_) =>
-      case _ =>
-        this.synchronized {
+      registered collectFirst { case (p, k) if path startsWith p => k } match {
+        case Some(_) =>
+          registered += path -> key
+          onRegister(key)
+        case _ =>
+          // Coerce a deep copy with toIndexedSeq
+          val excluded = registered.keySet.toIndexedSeq.toSet
+          registered += path -> key
           key.schedule(thread.runLoop)
           thread.signal()
+          submit(executor) {
+            allFiles ++= recursiveListFiles(path, excluded).map(f => f -> lastModified(f))
+            onRegister(key)
+          }
+      }
+      submit(cleanupExecutor) {
+        registered.synchronized(registered.toIndexedSeq) foreach {
+          case (p, k) => if ((p startsWith path) && (p != path)) cleanupKey(k)
         }
+      }
+      key
     }
-    registered foreach { case (p, k) => if ((p startsWith path) && (p != path)) cleanupKey(k) }
-    registered += path -> key
-    key
-  }
 
-  def onFileEvent(arg: (String, Int)): Unit = arg match {
+  private def onFileEvent(arg: (String, Int)): Unit = submit(executor)(arg match {
     case (folderName, flags) =>
-      import EventStreamFlags.{getFlags, MustScanSubDirs}
+      import EventStreamFlags.{MustScanSubDirs, getFlags}
       val path = new File(folderName).toPath
 
-      val folderFiles = if (getFlags(flags) contains MustScanSubDirs) recursiveListFiles(path) else {
+      val folderFiles = if (getFlags(flags) contains MustScanSubDirs) recursiveListFiles(path, Set.empty) else {
         path.toFile match {
           case p if p.isDirectory =>
             p.listFiles.collect { case f if !f.isDirectory => f.toPath }.toSet
@@ -87,40 +102,37 @@ class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: Wa
         }
       }
 
-      val files = allFiles get path match {
-        case Some(f) => f
-        case None =>
-          val map = mutable.Map.empty[Path, Long]
-          allFiles = allFiles + (path -> map)
-          map
-      }
+      registered.synchronized(registered get path) foreach { key =>
 
-      val key = registered.getOrElse(path, return)
+        @inline def signal(kind: WatchEvent.Kind[Path], file: Path, modified: Long): Unit = {
+          allFiles(file) = modified
+          createEvent(key, kind, file)
+        }
 
-      @inline def signal(kind: WatchEvent.Kind[Path], file: Path, modified: Long): Unit = {
-        files(file) = modified
-        createEvent(key, kind, file)
-      }
+        folderFiles foreach { file =>
+          val mtime = lastModified(file)
+          allFiles get file match {
+            case Some(m) if m != mtime && key.reportModifyEvents => signal(ENTRY_MODIFY, file, mtime)
+            case None if key.reportCreateEvents => signal(ENTRY_CREATE, file, mtime)
+            case _ => // This file hasn't changed or the event isn't reported.
+          }
+        }
 
-      folderFiles foreach { file =>
-        val mtime = lastModified(file)
-        files get file match {
-          case Some(m) if m != mtime && key.reportModifyEvents => signal(ENTRY_MODIFY, file, mtime)
-          case None if key.reportCreateEvents => signal(ENTRY_CREATE, file, mtime)
-          case _ => // This file hasn't changed or the event isn't reported.
+        allFiles.keySet.filter(_.getParent == path) diff folderFiles foreach { file =>
+          allFiles -= file
+          if (key.reportDeleteEvents) createEvent(key, ENTRY_DELETE, file)
         }
       }
-
-      files.keySet diff folderFiles foreach { file =>
-        files -= file
-        if (key.reportDeleteEvents) createEvent(key, ENTRY_DELETE, file)
-      }
-  }
+  })
 
   private def createEvent(key: MacOSXWatchKey, kind: WatchEvent.Kind[Path], file: Path): Unit = {
-    key.addEvent(Event(kind, 1, file))
-    if (!readyKeys.contains(key)) readyKeys.offer(key)
-    onOffer(key)
+    val event = Event(kind, 1, file)
+    key.addEvent(event)
+    onEvent(event)
+    if (!readyKeys.contains(key)) {
+      readyKeys.offer(key)
+      onOffer(key)
+    }
   }
 
   private def cleanupKey(key: MacOSXWatchKey): Unit = {
@@ -131,6 +143,8 @@ class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: Wa
 
   def isOpen: Boolean = open.get
 
+  private def submit[R](service: ExecutorService)(f: => R): Unit = { service.submit(() => f); () }
+
   private def createStream(path: Path): FSEventStreamRef = {
     import EventStreamCreateFlags._
     val values = Array(CFStringRef.toCFString(path.toFile.getAbsolutePath).getPointer)
@@ -139,12 +153,12 @@ class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: Wa
     FSEventStreamCreate(Ptr.NULL, callback, Ptr.NULL, pathsToWatch, sinceNow, latency, NoDefer)
   }
 
+  private[this] val allFiles = mutable.Map.empty[Path, Long]
   private[this] val latency = watchLatency.toMicros / 1e6
-  private[this] val thread: CFRunLoopThread = new CFRunLoopThread
-  private[this] val registered = mutable.Map.empty[Path, MacOSXWatchKey]
-
   private[this] val open = new AtomicBoolean(true)
   private[this] val readyKeys = new LinkedBlockingDeque[MacOSXWatchKey]
+  private[this] val registered = mutable.Map.empty[Path, MacOSXWatchKey]
+  private[this] val thread: CFRunLoopThread = new CFRunLoopThread
 
   private[this] lazy val callback: FSEventStreamCallback =
     (_: FSEventStreamRef, _: Ptr, numEvents: NativeLong, eventPaths: Ptr, flags: Ptr, _: Ptr) => {
@@ -152,21 +166,22 @@ class MacOSXWatchService(watchLatency: Duration, val queueSize: Int)(onOffer: Wa
       (eventPaths.getStringArray(0, count) zip flags.getIntArray(0, count)) foreach onFileEvent
     }
 
-  private[this] var allFiles = Map.empty[Path, mutable.Map[Path, Long]]
-
-
   @inline private def lastModified(o: Path): Long = o.toFile match {
     case f if f.exists => f.lastModified
     case _ => 0L
   }
 
-  @inline private def recursiveListFiles(path: Path): Set[Path] = path match {
-    case p if p.toFile.isDirectory =>
-      Try(Files.walk(path)).map {
-        _.filter(p => Try(Files.isRegularFile(p)) getOrElse false)
-          .collect(toSet[Path]).asScala.toSet
-      } getOrElse Set.empty
-    case p => Set(p)
+  @inline private def recursiveListFiles(path: Path, exclude: Set[Path]): Set[Path] = {
+    @tailrec def impl(paths: Seq[Path], exclude: Set[Path], accumulator: Set[Path]): Set[Path] = {
+      val (directories, files) = paths.flatMap(_.toFile.listFiles.map(_.toPath))
+        .filterNot(exclude)
+        .partition(_.toFile.isDirectory)
+      val newAccumulator = accumulator ++ files
+      if (directories.nonEmpty) impl(directories, exclude, newAccumulator) else newAccumulator
+    }
+
+    val (directories, files) = path.toFile.listFiles.partition(_.isDirectory)
+    impl(directories map (_.toPath) filterNot exclude, exclude, files.map(_.toPath).toSet)
   }
 }
 
