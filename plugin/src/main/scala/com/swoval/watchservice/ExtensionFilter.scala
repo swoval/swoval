ea938c32a43726491278fdb20bb9e61d515ea198
Ethan Atkins <ethan.atkins@gmail.com>
2017-12-18 11:54:24 -0800

    Add ExtensionFilter
    I found that the default extension filter in sbt that was constructed
    using the dsl: val filter = "*.scala" | "*.java" is quite slow,
    partially because it splits the file name for each extension. To improve
    performance, I add the less elegant ExtensionFilter.

---
+++ plugin/src/main/scala/com/swoval/watchservice/ExtensionFilter.scala
@@ -0,0 +1,17 @@
+package com.swoval.watchservice
+
+import java.io.File
+
+import sbt.io.FileFilter
+
+case class ExtensionFilter(extensions: String*) extends FileFilter {
+  val _extensions = extensions.toSet
+  override def accept(pathname: File): Boolean = {
+    val fileName = pathname.toString
+    val ext = fileName.indexOf('.') match {
+      case -1 => ""
+      case i  => fileName.substring(i + 1)
+    }
+    _extensions(ext)
+  }
+}
