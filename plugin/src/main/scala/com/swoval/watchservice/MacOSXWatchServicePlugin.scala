4dc26815b61216f2e75e06f1f2f2af9ab2ad11b7
Ethan Atkins <ethan.atkins@gmail.com>
2017-12-19 09:37:31 -0800

    Fix base directory handling
    The new file cache was not previously watching for files in the project
    base directory. This meant that if you had the plugin added globally,
    you could not actually load your project if it had any dependencies
    (e.g. project/Dependencies.scala). To fix this, I add the base directory
    to the list of directories that the file cache watches. Adding the base
    directory caused continuous execution thrashing though due to sbt 3439.
    Unfortunately, the fix that added the recursive boolean to Source only
    impacted the default watch service. In my case, I was still getting
    tripped up by the recursive search in the base directory because the
    recursive flag only applied to the getUnfilteredPaths method (which we
    decidedly do not want to call). To fix this, I had to override the
    watchSources to replace the base directory Source with a Source that
    only includes files in the base directory (and not recursively). It
    would be nice to fix sbt io upstream, but I'm not holding my breath.
    
    As a bonus, I fixed the assignment of the includeFilters. For some
    reason doing this with state sort of worked, but it's cleaner to assign
    them in the project settings.

--- plugin/src/main/scala/com/swoval/watchservice/MacOSXWatchServicePlugin.scala
+++ plugin/src/main/scala/com/swoval/watchservice/MacOSXWatchServicePlugin.scala
@@ -1,10 +1,12 @@
 package com.swoval.watchservice
 
-import com.swoval.watchservice.files.{ FileCache, NoCache }
-import sbt.Keys._
-import sbt.{ FileFilter => _, _ }
-import sbt.io.WatchService
 import java.io.FileFilter
+
+import com.swoval.watchservice.files.FileCache
+import sbt.Keys._
+import sbt.internal.io.Source
+import sbt.io.WatchService
+import sbt.{ FileFilter => _, _ }
 
 import scala.concurrent.duration._
 import scala.util.Properties
@@ -21,31 +23,38 @@ object MacOSXWatchServicePlugin extends AutoPlugin {
     lazy val fileCache = settingKey[FileCache]("Set the file cache to use.")
     lazy val useDefaultWatchService = settingKey[Boolean]("Use the built in sbt watch service.")
     lazy val useDefaultSourceList = settingKey[Boolean]("Use default sbt source list.")
+    lazy val useDefaultWatchSourceList = settingKey[Boolean]("Use default sbt watch source list.")
     lazy val useDefaultIncludeFilters = settingKey[Boolean]("Use default sbt include filters.")
   }
   import autoImport._
 
-  private[watchservice] lazy val _addedFilters =
-    AttributeKey[Boolean]("_SwovalAddedFiltersInternal", "Internal flag")
   private def defaultSourcesFor(conf: Configuration) = Def.task {
     def list(p: File) = fileCache.value.list(p.toPath, recursive = false, _ => false)
     (unmanagedSourceDirectories in conf).value foreach list
     (managedSourceDirectories in conf).value foreach list
     Classpaths.concat(unmanagedSources in conf, managedSources in conf).value
   }
-  private def cachedSourcesFor(conf: Configuration) = Def.task {
-    def filter(in: FileFilter, ex: FileFilter): FileFilter = f => in.accept(f) && !ex.accept(f)
-    def list(p: File, filter: FileFilter) = fileCache.value.list(p.toPath, recursive = true, filter)
-    val unmanagedFilter =
-      filter((includeFilter in unmanagedSources).value, (excludeFilter in unmanagedSources).value)
-    val managedFilter =
-      filter((includeFilter in managedSources).value, (excludeFilter in managedSources).value)
-    ((unmanagedSourceDirectories in conf).value flatMap (list(_, unmanagedFilter))) ++
-      ((managedSourceDirectories in conf).value flatMap (list(_, managedFilter)))
+  private def cachedSourcesFor(conf: Configuration, sourcesInBase: Boolean) = Def.task {
+    def filter(in: FileFilter, ex: FileFilter): FileFilter = f => { in.accept(f) && !ex.accept(f) }
+    def list(recursive: Boolean, filter: FileFilter) =
+      (f: File) => fileCache.value.list(f.toPath, recursive = recursive, filter)
+
+    val unmanagedDirs = (unmanagedSourceDirectories in conf).value
+    val unmanagedIncludeFilter = ((includeFilter in unmanagedSources) in conf).value
+    val unmanagedExcludeFilter = ((excludeFilter in unmanagedSources) in conf).value
+    val unmanagedFilter = filter(unmanagedIncludeFilter, unmanagedExcludeFilter)
+
+    val baseDirs = if (sourcesInBase) Seq((baseDirectory in conf).value) else Seq.empty
+    val baseFilter = filter(unmanagedIncludeFilter, unmanagedExcludeFilter)
+
+    val unmanaged = unmanagedDirs flatMap list(recursive = true, unmanagedFilter)
+    val base = baseDirs flatMap list(recursive = false, baseFilter)
+    unmanaged ++ base ++ (managedSources in conf).value
   }
 
   private def sourcesFor(conf: Configuration) = Def.taskDyn {
-    if (useDefaultSourceList.value) defaultSourcesFor(conf) else cachedSourcesFor(conf)
+    if (useDefaultSourceList.value) defaultSourcesFor(conf)
+    else cachedSourcesFor(conf, sourcesInBase.value)
   }
   override lazy val projectSettings: Seq[Def.Setting[_]] = super.projectSettings ++ Seq(
     pollInterval := 75.milliseconds, // sbt polls the watch service for events at this rate
@@ -54,24 +63,47 @@ object MacOSXWatchServicePlugin extends AutoPlugin {
     sources in Compile := sourcesFor(Compile).value,
     sources in Test := sourcesFor(Test).value,
     watchService := (() => createWatchService(watchLatency.value, watchQueueSize.value)),
+    includeFilter in unmanagedSources := {
+      if (useDefaultIncludeFilters.value) ("*.java" | "*.scala") && new SimpleFileFilter(_.isFile)
+      else ExtensionFilter("scala", "java") && new SimpleFileFilter(!_.isDirectory)
+    },
+    includeFilter in unmanagedJars := {
+      if (useDefaultIncludeFilters.value) "*.jar" | "*.so" | "*.dll" | "*.jnilib" | "*.zip"
+      else ExtensionFilter("jar", "so", "dll", "jnilib", "zip")
+    },
+    watchSources := {
+      val baseDir = baseDirectory.value
+      val include =
+        if (useDefaultWatchSourceList.value) (includeFilter in unmanagedSources).value
+        else
+          (includeFilter in unmanagedSources).value && new SimpleFileFilter(
+            f => f.toPath.getParent == baseDir.toPath)
+      val exclude = (excludeFilter in unmanagedSources).value
+      val baseSources =
+        if (sourcesInBase.value)
+          Seq(new Source(baseDir, include, exclude, recursive = false))
+        else Nil
+      getSources(unmanagedSourceDirectories, unmanagedSources).value ++
+        getSources(unmanagedResourceDirectories, unmanagedResources).value ++
+        baseSources
+    },
     fileCache := FileCache.default,
   )
+  private def getSources(key: SettingKey[Seq[File]], scope: TaskKey[Seq[File]]) = Def.task {
+    val dirs = (key in Compile).value ++ (key in Test).value
+    val include = (includeFilter in scope).value
+    val exclude = (excludeFilter in scope).value
+    dirs.map(b => new Source(b, include, exclude))
+  }
   override lazy val globalSettings = super.globalSettings ++ Seq(
     useDefaultWatchService := false,
     useDefaultSourceList := false,
-    onLoad := { s =>
-      val commands = s.definedCommands
+    useDefaultIncludeFilters := false,
+    useDefaultWatchSourceList := false,
+    onLoad := { state =>
+      val commands = state.definedCommands
         .filterNot(_ == BasicCommands.continuous) :+ Continuously.continuous
-      val extracted = Project.extract(s)
-      (extracted.getOpt[Boolean](useDefaultIncludeFilters) match {
-        case Some(false) if !s.attributes.contains(_addedFilters) =>
-          val filters = Seq(
-            includeFilter in unmanagedSources := ExtensionFilter("scala", "java"),
-            includeFilter in unmanagedJars := ExtensionFilter("jar", "so", "dll", "jnilib", "zip"),
-          )
-          extracted.append(filters, s.put(_addedFilters, true))
-        case _ => s
-      }).copy(definedCommands = commands)
+      state.copy(definedCommands = commands)
     },
   )
 }
