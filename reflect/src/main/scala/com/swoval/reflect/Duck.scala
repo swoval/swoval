f1fc04a4443bffb89cfac316beadd779f325fd8e
Ethan Atkins <ethan.atkins@gmail.com>
2018-02-17 13:28:24 -0800

    better

--- reflect/src/main/scala/com/swoval/reflect/Duck.scala
+++ reflect/src/main/scala/com/swoval/reflect/Duck.scala
@@ -1,78 +1,75 @@
 package com.swoval.reflect
 
 import scala.reflect.macros.blackbox
+import scala.reflect.macros.whitebox
 import scala.language.experimental.macros
 import scala.languageFeature.reflectiveCalls
 
-trait Duck[T, D] {
+trait Duck[T, D] extends Any {
   def duck(t: T): D
+}
+trait WeakDuck[T, D] extends Duck[T, D]
+object WeakDuck {
+  implicit def default[T, D]: WeakDuck[T, D] = macro DuckMacros.objectImpl[T, D, WeakDuck[_, _]]
 }
 
 object Duck {
-  implicit def default[T, D]: Duck[T, D] = macro Duck.impl[T, D]
-  def impl[T: c.WeakTypeTag, D: c.WeakTypeTag](c: blackbox.Context): c.Expr[Duck[T, D]] = {
+  implicit def default[T, D]: Duck[T, D] = macro DuckMacros.impl[T, D, Duck[_, _]]
+}
+trait LowPriorityDuck {}
+object DuckMacros {
+  def termName(c: blackbox.Context)(tpe: c.universe.Symbol) =
+    c.universe.TermName(c.freshName(tpe.fullName.split("\\.").last.toLowerCase))
+  def duck(
+      c: blackbox.Context)(tType: c.Type, dType: c.Type, name: c.Tree, body: c.Tree*): c.Tree = {
     import c.universe._
-    val (tType, dType) = (weakTypeOf[T], weakTypeOf[D])
-    if (!dType.typeSymbol.isClass || !dType.typeSymbol.asClass.isTrait) {
-      c.abort(c.enclosingPosition, s"Tried to duck type to non-trait type $dType")
-    }
-    def termName(tpe: Symbol) = TermName(c.freshName(tpe.fullName.split("\\.").last.toLowerCase))
-    def duck(body: c.Tree*) = q"""
-      new Duck[$tType, $dType] {
+    val sanitizedName = name.toString.split("\\.").last
+    q"""
+      new $name[$tType, $dType] {
         ..$body
-        override def toString() = "Duck[" + ${tType.typeSymbol.fullName} + ", " +
+        override def toString() = "" + $sanitizedName + "[" + ${tType.typeSymbol.fullName} + ", " +
           ${dType.typeSymbol.fullName} + "]"
       }
     """
-
-    val tree = tType match {
-      case t if t <:< dType =>
-        duck(q"override def duck(t: $tType): $dType = t")
-      case t if weakTypeOf[Object] <:< t && t <:< weakTypeOf[Any] =>
-        c.inferImplicitValue(weakTypeOf[reflectiveCalls], silent = true) match {
-          case q"" =>
-            c.warning(
-              c.enclosingPosition,
-              s"Generating duck type for $dType using reflective calls on $tType. To eliminate"
-                + " this warning, import scala.language.reflectiveCalls"
-            )
-          case _ =>
+  }
+  def objectImpl[O: c.WeakTypeTag, D: c.WeakTypeTag, DT <: Duck[_, _]: c.WeakTypeTag](
+      c: blackbox.Context): c.Expr[DT] = {
+    import c.universe._
+    val (tType, dType) = (weakTypeOf[O], weakTypeOf[D])
+    val tName = termName(c)(tType.typeSymbol)
+    val className = TermName(c.freshName("class"))
+    val methodsName = TermName(c.freshName("methods"))
+    val methods = q"""
+      val $methodsName = {
+        val classes = new scala.collection.mutable.ArrayBuffer[Class[_]]()
+        var $className: Class[_] = $tName.getClass
+        do {
+          classes += $className
+          $className = $className.getSuperclass
+        } while ($className != null)
+        classes.foldRight(Map.empty[String, Array[java.lang.reflect.Method]]) {
+          case (c, map) => map ++ c.getDeclaredMethods.groupBy(_.getName)
         }
-        val tName = TermName(c.freshName(t.typeSymbol.fullName.split("\\.").last.toLowerCase))
-        val className = TermName(c.freshName("class"))
-        val methodsName = TermName(c.freshName("methods"))
-        val methods =
-          q"""
-             val $methodsName = {
-               val classes = new scala.collection.mutable.ArrayBuffer[Class[_]]()
-               var $className: Class[_] = $tName.getClass
-               do {
-                 classes += $className
-                 $className = $className.getSuperclass
-               } while ($className != null)
-               classes.foldRight(Map.empty[String, Array[java.lang.reflect.Method]]) {
-                 case (c, map) => map ++ c.getDeclaredMethods.groupBy(_.getName)
-               }
-             }
-           """
-        val decls: Seq[c.Tree] = Seq(methods) ++ dType.decls
-          .filter(m => m.isMethod && !m.isConstructor)
-          .flatMap { d =>
-            val methodName = d.name.toString
-            val methodAlias = TermName(c.freshName(methodName))
-            val sig = d.typeSignature
-            val paramTypes = sig.paramLists.flatMap(_.map(_.typeSignature))
-            val paramNames = paramTypes.map(p =>
-              TermName(c.freshName(p.typeSymbol.fullName.split("\\.").last.toLowerCase)))
-            val params = paramNames zip paramTypes map { case (n, t) => q"val $n: $t" }
-            val boxed = (paramNames zip paramTypes).map { case (n, t) => Box(c)(n, t) }
-            val realParamNames = sig.paramLists.map(_.map(t => termName(t)))
-            val realParams = realParamNames.zip(sig.paramLists).map {
-              case (names, types) =>
-                names.zip(types) map { case (n, t) => q"val $n: ${t.typeSignature}" }
-            }
-            val rType = sig.finalResultType
-            val alias = q"""
+      }
+    """
+    val decls: Seq[c.Tree] = Seq(methods) ++ dType.decls
+      .filter(m => m.isMethod && !m.isConstructor)
+      .flatMap { d =>
+        val methodName = d.name.toString
+        val methodAlias = TermName(c.freshName(methodName))
+        val sig = d.typeSignature
+        val paramTypes = sig.paramLists.flatMap(_.map(_.typeSignature))
+        val paramNames = paramTypes.map(p =>
+          TermName(c.freshName(p.typeSymbol.fullName.split("\\.").last.toLowerCase)))
+        val params = paramNames zip paramTypes map { case (n, t) => q"val $n: $t" }
+        val boxed = (paramNames zip paramTypes).map { case (n, t) => Box(c)(n, t) }
+        val realParamNames = sig.paramLists.map(_.map(t => termName(c)(t)))
+        val realParams = realParamNames.zip(sig.paramLists).map {
+          case (names, types) =>
+            names.zip(types) map { case (n, t) => q"val $n: ${t.typeSignature}" }
+        }
+        val rType = sig.finalResultType
+        val alias = q"""
               lazy val $methodAlias: (..$paramTypes) => $rType = {
                 ($methodsName.get($methodName) match {
                   case Some(methods) =>
@@ -95,16 +92,39 @@ object Duck {
                 }
               }
             """
-            Seq(
-              alias,
-              q"""
+        Seq(
+          alias,
+          q"""
                 def ${TermName(methodName)}(...$realParams): ${sig.finalResultType} =
                   $methodAlias(..${realParamNames.flatten})
               """
-            )
-          }
-          .toSeq
-        duck(q"override def duck($tName: $tType): $dType = new $dType { ..$decls }")
+        )
+      }
+      .toSeq
+    val tree =
+      duck(c)(
+        tType,
+        dType,
+        tq"_root_.com.swoval.reflect.${weakTypeOf[DT].typeSymbol.name.toTypeName}",
+        q"override def duck($tName: $tType): $dType = new $dType { ..$decls }"
+      )
+    c.Expr[DT](tree)
+  }
+  def impl[T: c.WeakTypeTag, D: c.WeakTypeTag, DT <: Duck[_, _]: c.WeakTypeTag](
+      c: blackbox.Context): c.Expr[DT] = {
+    import c.universe._
+    val (tType, dType) = (weakTypeOf[T], weakTypeOf[D])
+    if (!dType.typeSymbol.isClass || !dType.typeSymbol.asClass.isTrait) {
+      c.abort(c.enclosingPosition, s"Tried to duck type to non-trait type $dType")
+    }
+    val duckName =
+      tq"_root_.com.swoval.reflect.${TypeName(weakTypeOf[DT].typeSymbol.name.toString)}"
+
+    val tree = tType match {
+      case t if t <:< dType =>
+        duck(c)(tType, dType, duckName, q"override def duck(t: $tType): $dType = t")
+      case t if weakTypeOf[Object] <:< t && t <:< weakTypeOf[Any] =>
+        c.abort(c.enclosingPosition, s"Can't generate duck type for generic type $tType")
       case t =>
         val methods = tType.decls.filter(d => d.isMethod && !d.isConstructor)
         val tName = TermName(c.freshName(t.typeSymbol.fullName.split("\\.").last.toLowerCase))
@@ -141,8 +161,12 @@ object Duck {
           case d              => d
         }
 
-        duck(q"override def duck($tName: $tType): $dType = new $dType { ..$decls }")
+        duck(c)(tType,
+                dType,
+                duckName,
+                q"override def duck($tName: $tType): $dType = new $dType { ..$decls }")
     }
-    c.Expr[Duck[T, D]](tree)
+    println(tree)
+    c.Expr[DT](tree)
   }
 }
--- reflect/src/test/scala/com/swoval/reflect/DuckTest.scala
+++ reflect/src/test/scala/com/swoval/reflect/DuckTest.scala
@@ -20,35 +20,37 @@ object DuckTest extends TestSuite {
             def foo(): Int = 3
           }
           implicitly[Duck[Bar.type, Foo]].duck(Bar).foo() ==> 3
+          println(implicitly[WeakDuck[Bar.type, Foo]])
+          WeakDuck.default[Bar.type, Foo].duck(Bar).foo() ==> 3
         }
       }
-      'default - {
-        trait Foo {
-          def foo(): Int = 3
-        }
-        'specified - {
-          object Bar {
-            def foo(): Int = 5
-          }
-          implicitly[Duck[Bar.type, Foo]].duck(Bar).foo() ==> 5
-        }
-        'unspecified - {
-          object Bar
-          implicitly[Duck[Bar.type, Foo]].duck(Bar).foo() ==> 3
-        }
-      }
+//      'default - {
+//        trait Foo {
+//          def foo(): Int = 3
+//        }
+//        'specified - {
+//          object Bar {
+//            def foo(): Int = 5
+//          }
+//          implicitly[Duck[Bar.type, Foo]].duck(Bar).foo() ==> 5
+//        }
+//        'unspecified - {
+//          object Bar
+//          implicitly[Duck[Bar.type, Foo]].duck(Bar).foo() ==> 3
+//        }
+//      }
     }
     'reflective - {
       'abstract - {
         'specified - {
-          trait Foo {
-            def foo(i: Int, y: Long): Long
-          }
-          abstract class Blah {
-            def foo(i: Int, y: Long): Long = i + y
-          }
-          object Bar extends Blah
-          implicitly[Duck[Object, Foo]].duck(Bar).foo(1, 3) ==> 4
+//          trait Foo {
+//            def foo(i: Int, y: Long): Long
+//          }
+//          abstract class Blah {
+//            def foo(i: Int, y: Long): Long = i + y
+//          }
+//          object Bar extends Blah
+//          implicitly[Duck[Object, Foo]].duck(Bar).foo(1, 3) ==> 4
 //          val foo: Foo = implicitly[Duck[Object, Foo]].duck(Bar)
 //          val n = 100000
 //          benchmark(n, "bar")(Bar.foo(1, 3))
