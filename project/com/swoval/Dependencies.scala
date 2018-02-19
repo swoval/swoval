bed9ba6d0e482ce4c51006d13d22c5640bb30a4b
Ethan Atkins <ethan.atkins@gmail.com>
2018-02-19 11:41:36 -0800

    Bump versions
    The apple events version wasn't actually published anywhere.

--- project/com/swoval/Dependencies.scala
+++ project/com/swoval/Dependencies.scala
@@ -5,14 +5,14 @@ import sbt.Keys._
 import sbt._
 
 object Dependencies {
-  val appleEventsVersion = "1.2.0"
+  val appleEventsVersion = "1.2.2"
   val sbtIO = "org.scala-sbt" %% "io" % "1.0.1"
   val scalagen = "com.mysema.scalagen" %% "scalagen" % "0.4.0"
   val scalaMacros = "org.scala-lang" % "scala-reflect"
   val utestVersion = "0.6.0"
   val utest = "com.lihaoyi" %% "utest" % utestVersion % "test"
 
-  def baseVersion: String = "1.2.0"
+  def baseVersion: String = "1.2.2"
   def ioScalaJS: SettingsDefinition = libraryDependencies += "io.scalajs" %%% "nodejs" % "0.4.2"
   def utestCrossMain = libraryDependencies += "com.lihaoyi" %%% "utest" % utestVersion
   def utestCrossTest = libraryDependencies += "com.lihaoyi" %%% "utest" % utestVersion % "test"
