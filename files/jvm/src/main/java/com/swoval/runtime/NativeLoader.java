2993d473d839e205621e1b503349916bd4fb624d
Ethan Atkins <ethan.atkins@gmail.com>
2018-07-03 12:49:27 -0700

    Add native library garbage collection
    There is no reliable way to unload a dll within a jvm session without
    using reflection techniques that are illegal in java >= 9. This makes it
    impossible to remove the temporary dll on Windows. Even on linux and
    mac, it probably isn't the best idea to delete the temporary shared
    library. Instead of deleting the file in the shutdown hook, I instead
    create a file with the suffix `.delete`. I then add a simple garbage
    collector that looks in the temporary library directory for all of the
    files that are marked with delete and removes them.

--- files/jvm/src/main/java/com/swoval/runtime/NativeLoader.java
+++ files/jvm/src/main/java/com/swoval/runtime/NativeLoader.java
@@ -2,18 +2,15 @@ package com.swoval.runtime;
 
 import com.swoval.files.QuickLister;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Iterator;
-import java.util.Vector;
 
 /**
  * Provides a static method for loading the swoval-files0 native library. This library provides a
@@ -25,87 +22,25 @@ public class NativeLoader {
   private static final String NATIVE_LIBRARY = "swoval-files0";
   private static final String lib = System.mapLibraryName(NATIVE_LIBRARY);
 
-  private static void loadPackaged(final String arch) throws IOException {
-    final Path swoval =
-        Files.createDirectories(
-            Paths.get(System.getProperty("java.io.tmpdir", "/tmp")).resolve("swoval-jni"));
-    final Path temp = Files.createTempDirectory(swoval, "native-lib").toRealPath();
-    final String resourcePath = "/native/" + arch + "/" + lib;
-    final URL url = NativeLoader.class.getResource(resourcePath);
-    if (url == null) {
-      String msg = "Native library " + lib + " (" + resourcePath + ") can't be loaded.";
-      deleteRecursive(temp);
-      throw new UnsatisfiedLinkError(msg);
-    }
-    final Path extractedPath = temp.resolve(lib);
-    try {
-      Files.write(extractedPath, Files.readAllBytes(Paths.get(url.toURI())));
-      System.load(extractedPath.toString());
-      ShutdownHooks.addHook(
-          2,
-          new Runnable() {
-            public void run() {
-              unloadNativeLibs();
-              try {
-                Files.deleteIfExists(extractedPath);
-                Files.deleteIfExists(temp);
-              } catch (IOException e) {
-                System.err.println("Error deleting temporary directory " + temp);
-              }
-            }
-          });
-
-    } catch (final URISyntaxException e) {
-      throw new RuntimeException(e); // Should be unreachable
-    } catch (final UnsatisfiedLinkError e) {
-      deleteRecursive(temp);
-      throw e;
-    }
-  }
-
-  /**
-   * This is necessary to unload the native shared library on windows. Windows won't let you delete
-   * a dll if it is in use, so it's necessary to unload it which is possible using by calling
-   * finalize on the {@link ClassLoader.NativeLibrary} objects.
-   */
-  @SuppressWarnings({"unchecked", "EmptyCatchBlock"})
-  private static void unloadNativeLibs() {
-    try {
-      ClassLoader classLoader = NativeLoader.class.getClassLoader();
-      Field field = ClassLoader.class.getDeclaredField("nativeLibraries");
-      field.setAccessible(true);
-      Vector<Object> libs = (Vector<Object>) field.get(classLoader);
-      Iterator it = libs.iterator();
-      while (it.hasNext()) {
-        Object object = it.next();
-        Field[] fs = object.getClass().getDeclaredFields();
-        for (int k = 0; k < fs.length; k++) {
-          if (fs[k].getName().equals("name")) {
-            fs[k].setAccessible(true);
-            Method finalize = object.getClass().getDeclaredMethod("finalize");
-            finalize.setAccessible(true);
-            finalize.invoke(object);
-          }
-        }
-      }
-    } catch (Exception e) {
-    }
-  }
-
-  private static void deleteRecursive(final Path path) {
+  @SuppressWarnings("EmptyCatchBlock")
+  private static void cleanupTask(final Path path) {
     try {
       Files.walkFileTree(
           path,
           new FileVisitor<Path>() {
             @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+            public FileVisitResult preVisitDirectory(
+                final Path dir, final BasicFileAttributes attrs) {
               return FileVisitResult.CONTINUE;
             }
 
             @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs)
                 throws IOException {
-              Files.delete(file);
+              if (file.toString().endsWith(".delete")) {
+                Files.deleteIfExists(Paths.get(file.toString().replaceAll(".delete", "")));
+                Files.deleteIfExists(file);
+              }
               return FileVisitResult.CONTINUE;
             }
 
@@ -117,13 +52,62 @@ public class NativeLoader {
             @Override
             public FileVisitResult postVisitDirectory(Path dir, IOException exc)
                 throws IOException {
-              Files.delete(dir);
+              try {
+                Files.deleteIfExists(dir);
+              } catch (final DirectoryNotEmptyException e) {
+              } catch (final IOException e) {
+                throw e;
+              }
               return FileVisitResult.CONTINUE;
             }
           });
-    } catch (IOException e) {
-      System.err.println("Error deleting " + path + ": " + e);
+    } catch (final IOException e) {
     }
+  }
+
+  private static void loadPackaged(final String arch) throws IOException {
+    final Path swoval =
+        Files.createDirectories(
+            Paths.get(System.getProperty("java.io.tmpdir", "/tmp")).resolve("swoval-jni"));
+    final String resourcePath = "/native/" + arch + "/" + lib;
+    final URL url = NativeLoader.class.getResource(resourcePath);
+    if (url == null) {
+      String msg = "Native library " + lib + " (" + resourcePath + ") can't be loaded.";
+      throw new UnsatisfiedLinkError(msg);
+    }
+    final Path extractedPath = Files.createTempFile(swoval, "", "-" + lib);
+    try {
+      Files.write(extractedPath, Files.readAllBytes(Paths.get(url.toURI())));
+      System.load(extractedPath.toString());
+      ShutdownHooks.addHook(
+          2,
+          new Runnable() {
+            public void run() {
+              try {
+                Files.createFile(
+                    swoval.resolve(extractedPath.getFileName().toString() + ".delete"));
+              } catch (IOException e) {
+                System.err.println("Error marking " + extractedPath + " for deletion: " + e);
+              }
+            }
+          });
+
+    } catch (final URISyntaxException e) {
+      throw new RuntimeException(e); // Should be unreachable
+    } catch (final UnsatisfiedLinkError e) {
+
+      Files.deleteIfExists(extractedPath);
+      throw e;
+    }
+    final Thread thread =
+        new Thread("com.swoval.runtime.NativeLoader.cleanupThread") {
+          @Override
+          public void run() {
+            cleanupTask(swoval);
+          }
+        };
+    thread.setDaemon(true);
+    thread.start();
   }
 
   public static void loadPackaged() throws IOException, UnsatisfiedLinkError {
