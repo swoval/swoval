858813e3876e86871d94fe425f9053a99beae183
Ethan Atkins <ethan.atkins@gmail.com>
2018-07-03 18:11:43 -0700

    Add shutdown hooks javadoc

--- files/jvm/src/main/java/com/swoval/runtime/ShutdownHooks.java
+++ files/jvm/src/main/java/com/swoval/runtime/ShutdownHooks.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 
+/**
+ * Provides an api for adding shutdown hooks. This exists because there is no {@link
+ * java.lang.Runtime#addShutdownHook} implementation available on scala.js. The hooks may be added
+ * with a priority that controls the order in which the hooks run. Lower values run first.
+ */
 public class ShutdownHooks {
   static {
     Runtime.getRuntime()
@@ -40,6 +45,11 @@ public class ShutdownHooks {
     }
   }
 
+  /**
+   * Add a hook to run at shutdown.
+   * @param priority Controls the ordering of this hook. Lower values run first.
+   * @param runnable The shutdown task to run
+   */
   public static void addHook(final int priority, final Runnable runnable) {
     hooks.add(new Hook(priority, runnable));
   }
