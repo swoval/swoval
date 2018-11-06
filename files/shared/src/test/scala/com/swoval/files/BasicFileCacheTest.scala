swoval/swoval

commit 464e33961afe73aa8611c605a2300c042f56bad9

Adjust overlapping directory semantics
I discovered some edge cases that could occur if a base directory was
registered non-recursively and then one of its immediate children, say
foo,  is separately registered. When this would happen, the base
registration would effectively be extended to cover the child, up to the
child's max depth. This isn't correct, though, because that means that
the directory would also include the other children, up to the max depth
of foo. To fix this, I create the directories with a filter that allows
a directory to include only those files accepted by the filter.
Conveniently, I already had such a filter, the DirectoryRegistry.

I also had to clean up the list of directories in the
FileCacheDirectoryTree whenever there is an update, because it's
possible that two independently registered directories could have a new
overlap and we only want a single entry for the least specific path when
that occurs.

I also removed the package private cachedUpdatable static method from
FileTreeDataViews to keep that class smaller.

files/js/src/main/scala/com/swoval/files/FileCacheDirectoryTree.scala
@@ -72,6 +72,9 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
   private val directoryRegistry: DirectoryRegistry =
     new DirectoryRegistryImpl()
 
+  private val filter: Filter[TypedPath] =
+    DirectoryRegistries.toTypedPathFilter(directoryRegistry)
+
   private val observers: CacheObservers[T] = new CacheObservers()
 
   private val followLinks: Boolean = symlinkWatcher != null
@@ -138,12 +141,26 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
 
   private def find(path: Path): CachedDirectory[T] = {
     var foundDir: CachedDirectory[T] = null
-    val it: Iterator[CachedDirectory[T]] = directories.values.iterator()
-    while (it.hasNext) {
+    val dirs: List[CachedDirectory[T]] = directories.values
+    Collections.sort(
+      dirs,
+      new Comparator[CachedDirectory[T]]() {
+        override def compare(left: CachedDirectory[T], right: CachedDirectory[T]): Int = // Descending order so that we find the most specific path
+          right.getPath.compareTo(left.getPath)
+      }
+    )
+    val it: Iterator[CachedDirectory[T]] = dirs.iterator()
+    while (it.hasNext && foundDir == null) {
       val dir: CachedDirectory[T] = it.next()
-      if (path.startsWith(dir.getPath) &&
-          (foundDir == null || dir.getPath.startsWith(foundDir.getPath))) {
-        foundDir = dir
+      if (path.startsWith(dir.getPath)) {
+        if (dir.getMaxDepth == java.lang.Integer.MAX_VALUE || path == dir.getPath) {
+          foundDir = dir
+        } else {
+          val depth: Int = dir.getPath.relativize(path).getNameCount - 1
+          if (depth <= dir.getMaxDepth) {
+            foundDir = dir
+          }
+        }
       }
     }
     foundDir
@@ -185,15 +202,10 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
           } else if (pendingFiles.remove(path)) {
             try {
               var cachedDirectory: CachedDirectory[T] = null
-              try cachedDirectory = FileTreeDataViews.cachedUpdatable[T](
-                path,
-                converter,
-                directoryRegistry.maxDepthFor(path),
-                followLinks)
+              try cachedDirectory = newCachedDirectory(path, directoryRegistry.maxDepthFor(path))
               catch {
                 case nde: NotDirectoryException =>
-                  cachedDirectory = FileTreeDataViews
-                    .cachedUpdatable[T](path, converter, -1, followLinks)
+                  cachedDirectory = newCachedDirectory(path, -1)
 
               }
               val previous: CachedDirectory[T] =
@@ -309,45 +321,25 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
         while (it.hasNext && existing == null) {
           val dir: CachedDirectory[T] = it.next()
           if (path.startsWith(dir.getPath)) {
-            val depth: Int =
-              if (path == dir.getPath) 0
-              else (dir.getPath.relativize(path).getNameCount - 1)
-            if (dir.getMaxDepth == java.lang.Integer.MAX_VALUE || maxDepth < dir.getMaxDepth - depth) {
-              existing = dir
-            } else if (depth <= dir.getMaxDepth) {
-              dir.close()
-              try {
-                val md: Int =
-                  if (maxDepth < java.lang.Integer.MAX_VALUE - depth - 1)
-                    maxDepth + depth + 1
-                  else java.lang.Integer.MAX_VALUE
-                existing = FileTreeDataViews
-                  .cachedUpdatable[T](dir.getPath, converter, md, followLinks)
-                directories.put(dir.getPath, existing)
-              } catch {
-                case e: IOException => existing = null
-
-              }
-            }
+            existing = dir
           }
         }
         var dir: CachedDirectory[T] = null
         if (existing == null) {
+          val view: FileTreeView = FileTreeViews.getDefault(followLinks)
           try {
-            try dir = FileTreeDataViews
-              .cachedUpdatable[T](path, converter, maxDepth, followLinks)
+            try dir =
+              newCachedDirectory(path, if (maxDepth == -1) -1 else java.lang.Integer.MAX_VALUE)
             catch {
               case e: NotDirectoryException =>
-                dir = FileTreeDataViews
-                  .cachedUpdatable[T](path, converter, -1, followLinks)
+                dir = newCachedDirectory(path, -1)
 
             }
             directories.put(path, dir)
           } catch {
             case e: NoSuchFileException => {
               pendingFiles.add(path)
-              dir = FileTreeDataViews
-                .cachedUpdatable[T](path, converter, -1, followLinks)
+              dir = newCachedDirectory(path, -1)
             }
 
           }
@@ -355,11 +347,32 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
           existing.update(TypedPaths.get(path))
           dir = existing
         }
+        cleanupDirectories(path, maxDepth)
         dir
       } finally directories.unlock()
     } else {
       null
     }
+
+  private def cleanupDirectories(path: Path, maxDepth: Int): Unit = {
+    val it: Iterator[CachedDirectory[T]] = directories.values.iterator()
+    val toRemove: List[Path] = new ArrayList[Path]()
+    while (it.hasNext) {
+      val dir: CachedDirectory[T] = it.next()
+      if (dir.getPath.startsWith(path) && dir.getPath != path) {
+        if (maxDepth == java.lang.Integer.MAX_VALUE) {
+          toRemove.add(dir.getPath)
+        } else {
+          val depth: Int = path.relativize(dir.getPath).getNameCount - 1
+          if (maxDepth - depth >= dir.getMaxDepth) {
+            toRemove.add(dir.getPath)
+          }
+        }
+      }
+    }
+    val removeIterator: Iterator[Path] = toRemove.iterator()
+    while (removeIterator.hasNext) directories.remove(removeIterator.next())
+  }
 
   private def addCallback(callbacks: List[Callback],
                           symlinks: List[TypedPath],
@@ -414,7 +427,8 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
             result.add(dir.getEntry)
             result
           } else {
-            dir.listEntries(path, maxDepth, filter)
+            val depth: Int = directoryRegistry.maxDepthFor(path)
+            dir.listEntries(path, if (depth < maxDepth) depth else maxDepth, filter)
           }
         }
       } finally directories.unlock()
@@ -462,5 +476,13 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
     } else {
       Collections.emptyList()
     }
+
+  private def newCachedDirectory(path: Path, depth: Int): CachedDirectory[T] =
+    new CachedDirectoryImpl[T](path,
+                               path,
+                               converter,
+                               depth,
+                               filter,
+                               FileTreeViews.getDefault(followLinks)).init()
 
 }

files/jvm/src/main/java/com/swoval/files/FileCacheDirectoryTree.java
@@ -80,6 +80,7 @@ class FileCachePendingFiles extends Lockable {
 
 class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<T> {
   private final DirectoryRegistry directoryRegistry = new DirectoryRegistryImpl();
+  private final Filter<TypedPath> filter = DirectoryRegistries.toTypedPathFilter(directoryRegistry);
   private final Converter<T> converter;
   private final CacheObservers<T> observers = new CacheObservers<>();
   private final Executor callbackExecutor;
@@ -177,12 +178,28 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
 
   private CachedDirectory<T> find(final Path path) {
     CachedDirectory<T> foundDir = null;
-    final Iterator<CachedDirectory<T>> it = directories.values().iterator();
-    while (it.hasNext()) {
+    final List<CachedDirectory<T>> dirs = directories.values();
+    Collections.sort(
+        dirs,
+        new Comparator<CachedDirectory<T>>() {
+          @Override
+          public int compare(final CachedDirectory<T> left, final CachedDirectory<T> right) {
+            // Descending order so that we find the most specific path
+            return right.getPath().compareTo(left.getPath());
+          }
+        });
+    final Iterator<CachedDirectory<T>> it = dirs.iterator();
+    while (it.hasNext() && foundDir == null) {
       final CachedDirectory<T> dir = it.next();
-      if (path.startsWith(dir.getPath())
-          && (foundDir == null || dir.getPath().startsWith(foundDir.getPath()))) {
-        foundDir = dir;
+      if (path.startsWith(dir.getPath())) {
+        if (dir.getMaxDepth() == Integer.MAX_VALUE || path.equals(dir.getPath())) {
+          foundDir = dir;
+        } else {
+          int depth = dir.getPath().relativize(path).getNameCount() - 1;
+          if (depth <= dir.getMaxDepth()) {
+            foundDir = dir;
+          }
+        }
       }
     }
     return foundDir;
@@ -227,12 +244,9 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
             try {
               CachedDirectory<T> cachedDirectory;
               try {
-                cachedDirectory =
-                    FileTreeDataViews.<T>cachedUpdatable(
-                        path, converter, directoryRegistry.maxDepthFor(path), followLinks);
+                cachedDirectory = newCachedDirectory(path, directoryRegistry.maxDepthFor(path));
               } catch (final NotDirectoryException nde) {
-                cachedDirectory =
-                    FileTreeDataViews.<T>cachedUpdatable(path, converter, -1, followLinks);
+                cachedDirectory = newCachedDirectory(path, -1);
               }
               final CachedDirectory<T> previous = directories.put(path, cachedDirectory);
               if (previous != null) previous.close();
@@ -333,7 +347,7 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
             dirs,
             new Comparator<CachedDirectory<T>>() {
               @Override
-              public int compare(CachedDirectory<T> left, CachedDirectory<T> right) {
+              public int compare(final CachedDirectory<T> left, final CachedDirectory<T> right) {
                 return left.getPath().compareTo(right.getPath());
               }
             });
@@ -342,51 +356,56 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
         while (it.hasNext() && existing == null) {
           final CachedDirectory<T> dir = it.next();
           if (path.startsWith(dir.getPath())) {
-            final int depth =
-                path.equals(dir.getPath())
-                    ? 0
-                    : (dir.getPath().relativize(path).getNameCount() - 1);
-            if (dir.getMaxDepth() == Integer.MAX_VALUE || maxDepth < dir.getMaxDepth() - depth) {
-              existing = dir;
-            } else if (depth <= dir.getMaxDepth()) {
-              dir.close();
-              try {
-                final int md =
-                    maxDepth < Integer.MAX_VALUE - depth - 1
-                        ? maxDepth + depth + 1
-                        : Integer.MAX_VALUE;
-                existing =
-                    FileTreeDataViews.<T>cachedUpdatable(dir.getPath(), converter, md, followLinks);
-                directories.put(dir.getPath(), existing);
-              } catch (IOException e) {
-                existing = null;
-              }
-            }
+            existing = dir;
           }
         }
         CachedDirectory<T> dir;
         if (existing == null) {
+          final FileTreeView view = FileTreeViews.getDefault(followLinks);
           try {
             try {
-              dir = FileTreeDataViews.<T>cachedUpdatable(path, converter, maxDepth, followLinks);
+              dir = newCachedDirectory(path, maxDepth == -1 ? -1 : Integer.MAX_VALUE);
             } catch (final NotDirectoryException e) {
-              dir = FileTreeDataViews.<T>cachedUpdatable(path, converter, -1, followLinks);
+              dir = newCachedDirectory(path, -1);
             }
             directories.put(path, dir);
           } catch (final NoSuchFileException e) {
             pendingFiles.add(path);
-            dir = FileTreeDataViews.<T>cachedUpdatable(path, converter, -1, followLinks);
+            dir = newCachedDirectory(path, -1);
           }
         } else {
           existing.update(TypedPaths.get(path));
           dir = existing;
         }
+        cleanupDirectories(path, maxDepth);
         return dir;
       } finally {
         directories.unlock();
       }
     } else {
       return null;
+    }
+  }
+
+  private void cleanupDirectories(final Path path, final int maxDepth) {
+    final Iterator<CachedDirectory<T>> it = directories.values().iterator();
+    final List<Path> toRemove = new ArrayList<>();
+    while (it.hasNext()) {
+      final CachedDirectory<T> dir = it.next();
+      if (dir.getPath().startsWith(path) && !dir.getPath().equals(path)) {
+        if (maxDepth == Integer.MAX_VALUE) {
+          toRemove.add(dir.getPath());
+        } else {
+          int depth = path.relativize(dir.getPath()).getNameCount() - 1;
+          if (maxDepth - depth >= dir.getMaxDepth()) {
+            toRemove.add(dir.getPath());
+          }
+        }
+      }
+    }
+    final Iterator<Path> removeIterator = toRemove.iterator();
+    while (removeIterator.hasNext()) {
+      directories.remove(removeIterator.next());
     }
   }
 
@@ -452,7 +471,8 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
             result.add(dir.getEntry());
             return result;
           } else {
-            return dir.listEntries(path, maxDepth, filter);
+            final int depth = directoryRegistry.maxDepthFor(path);
+            return dir.listEntries(path, depth < maxDepth ? depth : maxDepth, filter);
           }
         }
       } finally {
@@ -511,5 +531,12 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
     } else {
       return Collections.emptyList();
     }
+  }
+
+  private CachedDirectory<T> newCachedDirectory(final Path path, final int depth)
+      throws IOException {
+    return new CachedDirectoryImpl<T>(
+            path, path, converter, depth, filter, FileTreeViews.getDefault(followLinks))
+        .init();
   }
 }

files/shared/src/test/scala/com/swoval/files/BasicFileCacheTest.scala
@@ -133,32 +133,59 @@ trait BasicFileCacheTest extends TestSuite with FileCacheTest {
             }
           }
         }
-        'overlap - withTempDirectory { dir =>
+        'adjacent - withTempDirectory { dir =>
           withTempDirectory(dir) { subdir =>
             withTempDirectory(subdir) { nestedSubdir =>
               withTempFile(nestedSubdir) { file =>
-                val latch = new CountDownLatch(1)
-                usingAsync(simpleCache((e: Entry[Path]) =>
-                  if (e.getPath.endsWith("deep")) latch.countDown())) { c =>
-                  c.register(dir, 1)
-                  c.ls(dir) === Set(subdir, nestedSubdir)
-                  c.register(nestedSubdir, 0)
-                  c.ls(dir) === Set(subdir, nestedSubdir, file)
-                  val deep = Files.createDirectory(nestedSubdir.resolve("deep"))
-                  val deepFile = Files.createFile(deep.resolve("file"))
-                  latch.waitFor(DEFAULT_TIMEOUT) {
-                    var i = 0
-                    while (!Files.exists(deepFile) && i < 1000) {
-                      i += 1
+                using(simpleCache((e: Entry[Path]) => {})) { c =>
+                  c.register(dir, 0)
+                  c.ls(dir) === Set(subdir)
+                  c.register(subdir, Integer.MAX_VALUE)
+                  c.ls(dir) === Set(subdir)
+                  c.ls(subdir) === Set(nestedSubdir, file)
+                }
+              }
+            }
+          }
+        }
+        'overlap - {
+          'infinite - withTempDirectory { dir =>
+            withTempDirectory(dir) { subdir =>
+              withTempDirectory(subdir) { nestedSubdir =>
+                withTempFile(nestedSubdir) { file =>
+                  using(simpleCache((e: Entry[Path]) => {})) { c =>
+                    c.register(dir, 0)
+                    c.ls(dir) === Set(subdir)
+                    c.register(subdir, Integer.MAX_VALUE)
+                    c.ls(dir) === Set(subdir)
+                    c.ls(subdir) === Set(nestedSubdir, file)
+                    c.register(dir, Integer.MAX_VALUE)
+                    c.ls(dir).sorted === Seq(subdir, nestedSubdir, file)
+                  }
+                }
+              }
+            }
+          }
+          'finite - withTempDirectory { dir =>
+            withTempDirectory(dir) { subdir =>
+              withTempDirectory(subdir) { nestedSubdir =>
+                withTempDirectory(nestedSubdir) { nestedNestedSubdir =>
+                  val file = nestedSubdir.resolve("file")
+                  val latch = new CountDownLatch(1)
+                  usingAsync(simpleCache((e: Entry[Path]) => {
+                    if (e.getPath == file) latch.countDown()
+                  })) { c =>
+                    c.register(dir, 0)
+                    c.ls(dir) === Set(subdir)
+                    c.register(subdir, Integer.MAX_VALUE)
+                    c.ls(dir) === Set(subdir)
+                    c.ls(subdir) === Set(nestedSubdir, nestedNestedSubdir)
+                    c.register(dir, 1)
+                    c.ls(dir).sorted === Seq(subdir, nestedSubdir)
+                    file.createFile()
+                    latch.waitFor(DEFAULT_TIMEOUT) {
+                      c.ls(subdir).sorted === Seq(nestedSubdir, nestedNestedSubdir, file)
                     }
-                    val existing =
-                      FileTreeViews
-                        .list(dir, Integer.MAX_VALUE, AllPass)
-                        .asScala
-                        .map(_.getPath)
-                        .toSet
-                    existing === Set(subdir, nestedSubdir, file, deep, deepFile)
-                    c.ls(dir) === Set(subdir, nestedSubdir, file, deep)
                   }
                 }
               }
@@ -372,7 +399,7 @@ trait BasicFileCacheTest extends TestSuite with FileCacheTest {
             using(simpleCache((_: Entry[Path]) => {})) { c =>
               c.register(dir, -1)
               c.ls(dir) === Seq(dir)
-              c.list(dir, 0, AllPass).asScala.toSeq === Seq(TypedPaths.get(dir))
+              c.list(dir, 0, AllPass).asScala.toSeq.map(_.getPath).headOption ==> Some(dir)
             }
           }
           'nonExistent - using(simpleCache((_: Entry[Path]) => {})) { c =>

files/js/src/main/scala/com/swoval/files/DirectoryRegistry.scala
@@ -28,6 +28,16 @@ trait DirectoryRegistry extends Filter[Path] with AutoCloseable {
 
 }
 
+object DirectoryRegistries {
+
+  def toTypedPathFilter(registry: DirectoryRegistry): Filter[TypedPath] =
+    new Filter[TypedPath]() {
+      override def accept(typedPath: TypedPath): Boolean =
+        registry.accept(typedPath.getPath)
+    }
+
+}
+
 object DirectoryRegistryImpl {
 
   private class RegisteredDirectory(val path: Path, val maxDepth: Int) {
@@ -116,7 +126,7 @@ class DirectoryRegistryImpl extends DirectoryRegistry {
 
   override def accept(path: Path): Boolean = acceptImpl(path, false)
 
-  def acceptPrefix(path: Path): Boolean = acceptImpl(path, true)
+  override def acceptPrefix(path: Path): Boolean = acceptImpl(path, true)
 
   override def close(): Unit = {
     registeredDirectoriesByPath.clear()

files/js/src/main/scala/com/swoval/files/FileTreeDataViews.scala
@@ -11,28 +11,6 @@ import java.nio.file.Path
 object FileTreeDataViews {
 
   /**
-   * Make a new CachedDirectory with a cache entries created by {@code converter}.
-   *
-   * @param path the path to cache
-   * @param converter a function to create the cache value for each path
-   * @param depth determines how many levels of children of subdirectories to include in the results
-   * @param followLinks sets whether or not to treat symbolic links whose targets as directories or
-   *     files
-   * @tparam T the cache value type
-   * @return a directory with entries of type T.
-   */
-  def cachedUpdatable[T <: AnyRef](path: Path,
-                                   converter: Converter[T],
-                                   depth: Int,
-                                   followLinks: Boolean): CachedDirectory[T] =
-    new CachedDirectoryImpl(path,
-                            path,
-                            converter,
-                            depth,
-                            Filters.AllPass,
-                            FileTreeViews.getDefault(followLinks)).init()
-
-  /**
    * Make a new [[DirectoryView]] that caches the file tree but has no data value associated
    * with each value.
    *
@@ -48,7 +26,12 @@ object FileTreeDataViews {
                           converter: Converter[T],
                           depth: Int,
                           followLinks: Boolean): DirectoryDataView[T] =
-    cachedUpdatable(path, converter, depth, followLinks)
+    new CachedDirectoryImpl(path,
+                            path,
+                            converter,
+                            depth,
+                            Filters.AllPass,
+                            FileTreeViews.getDefault(followLinks)).init()
 
   /**
    * Container class for [[CachedDirectoryImpl]] entries. Contains both the path to which the

files/jvm/src/main/java/com/swoval/files/DirectoryRegistry.java
@@ -25,6 +25,19 @@ interface DirectoryRegistry extends Filter<Path>, AutoCloseable {
   void close();
 }
 
+class DirectoryRegistries {
+  private DirectoryRegistries() {}
+
+  static Filter<TypedPath> toTypedPathFilter(final DirectoryRegistry registry) {
+    return new Filter<TypedPath>() {
+      @Override
+      public boolean accept(final TypedPath typedPath) {
+        return registry.accept(typedPath.getPath());
+      }
+    };
+  }
+}
+
 class DirectoryRegistryImpl implements DirectoryRegistry {
   private final Map<Path, RegisteredDirectory> registeredDirectoriesByPath = new HashMap<>();
   private final Object lock = new Object();
@@ -99,6 +112,7 @@ class DirectoryRegistryImpl implements DirectoryRegistry {
     return acceptImpl(path, false);
   }
 
+  @Override
   public boolean acceptPrefix(final Path path) {
     return acceptImpl(path, true);
   }

files/jvm/src/main/java/com/swoval/files/FileTreeDataViews.java
@@ -13,26 +13,6 @@ public class FileTreeDataViews {
   private FileTreeDataViews() {}
 
   /**
-   * Make a new CachedDirectory with a cache entries created by {@code converter}.
-   *
-   * @param path the path to cache
-   * @param converter a function to create the cache value for each path
-   * @param depth determines how many levels of children of subdirectories to include in the results
-   * @param followLinks sets whether or not to treat symbolic links whose targets as directories or
-   *     files
-   * @param <T> the cache value type
-   * @return a directory with entries of type T.
-   * @throws IOException when an error is encountered traversing the directory.
-   */
-  static <T> CachedDirectory<T> cachedUpdatable(
-      final Path path, final Converter<T> converter, final int depth, final boolean followLinks)
-      throws IOException {
-    return new CachedDirectoryImpl<>(
-            path, path, converter, depth, Filters.AllPass, FileTreeViews.getDefault(followLinks))
-        .init();
-  }
-
-  /**
    * Make a new {@link DirectoryView} that caches the file tree but has no data value associated
    * with each value.
    *
@@ -48,7 +28,9 @@ public class FileTreeDataViews {
   public static <T> DirectoryDataView<T> cached(
       final Path path, final Converter<T> converter, final int depth, final boolean followLinks)
       throws IOException {
-    return cachedUpdatable(path, converter, depth, followLinks);
+    return new CachedDirectoryImpl<>(
+            path, path, converter, depth, Filters.AllPass, FileTreeViews.getDefault(followLinks))
+        .init();
   }
 
   /**

commit 72e986240a792807d9e1fa9f89d2e39a84bd3224

Fix non-recursive directory bug
I introduced a bug in 464e33961afe73aa8611c605a2300c042f56bad9 where any
directory that was added with a non negative one valued maxDepth
parameter would be created with a maxDepth of Integer.MAX_VALUE. It
would not actually include the recursive files because of the filter
that applies the DirectoryRegister.maxDepth parameter so we ended up in
an inconsistent state where the CachedDirectory purported to have depth
Integer.MAX_VALUE, but, in practice, had depth 0. When this directory
was the base directory of an sbt project, this caused problems because
we'd only see the files in the base directory and none of the children.

The new regression test fails if run without the FileCacheDirectoryTree
change.

files/js/src/main/scala/com/swoval/files/FileCacheDirectoryTree.scala
@@ -337,8 +337,7 @@ class FileCacheDirectoryTree[T <: AnyRef](private val converter: Converter[T],
         var dir: CachedDirectory[T] = null
         if (existing == null) {
           try {
-            try dir =
-              newCachedDirectory(path, if (maxDepth == -1) -1 else java.lang.Integer.MAX_VALUE)
+            try dir = newCachedDirectory(path, maxDepth)
             catch {
               case e: NotDirectoryException =>
                 dir = newCachedDirectory(path, -1)

files/jvm/src/main/java/com/swoval/files/FileCacheDirectoryTree.java
@@ -377,7 +377,7 @@ class FileCacheDirectoryTree<T> implements ObservableCache<T>, FileTreeDataView<
         if (existing == null) {
           try {
             try {
-              dir = newCachedDirectory(path, maxDepth == -1 ? -1 : Integer.MAX_VALUE);
+              dir = newCachedDirectory(path, maxDepth);
             } catch (final NotDirectoryException e) {
               dir = newCachedDirectory(path, -1);
             }

files/shared/src/test/scala/com/swoval/files/BasicFileCacheTest.scala
@@ -192,6 +192,21 @@ trait BasicFileCacheTest extends TestSuite with FileCacheTest {
               }
             }
           }
+          'deeplyNested - withTempDirectory { dir =>
+            withTempDirectory(dir) { subdir =>
+              val nestedSubdir = Files.createDirectories(subdir.resolve("nested"))
+              val deeplyNestedSubdir =
+                Files.createDirectories(subdir.resolve("very").resolve("deeply").resolve("nested"))
+              val file = deeplyNestedSubdir.resolve("file").createFile()
+              using(simpleCache((e: Entry[Path]) => {})) { c =>
+                c.register(dir, 0)
+                c.register(nestedSubdir, Integer.MAX_VALUE)
+                c.ls(deeplyNestedSubdir) ==> Nil
+                c.register(deeplyNestedSubdir, Integer.MAX_VALUE)
+                c.ls(deeplyNestedSubdir).sorted === Seq(file)
+              }
+            }
+          }
         }
         'holes - {
           'limit - withTempDirectory { dir =>