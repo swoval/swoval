d008753f9422e444d20f7b10d8086521d7d9200e
Ethan Atkins <ethan.atkins@gmail.com>
2019-02-14 15:04:25 -0800

    Remove unused Defer class

--- files/js/src/test/scala/com/swoval/files/Defer.scala
+++
@@ -1,8 +0,0 @@
-package com.swoval.files
-
-import scala.concurrent.duration.FiniteDuration
-import scala.scalajs.js.timers._
-
-object Defer {
-  def apply[T](duration: FiniteDuration)(thunk: => T): Unit = setTimeout(duration)(thunk)
-}
